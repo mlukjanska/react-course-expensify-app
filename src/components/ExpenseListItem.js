f627c94ab4fb4be7f5266787f7de23ba0eafffaf
Margarita Lukjanska <mlukjanska@users.noreply.github.com>
2018-10-21 00:34:51 +0100

    Setup formatting for amount and createdat

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,14 +1,27 @@
 // Export a stateless functional component
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
+import '../locales/eu.js';
 
-const ExpenseListItem = ({id, description, amount, createdAt}) => (
-    <div>
-      <Link to={`/edit/${id}`}>
-        <h3>{description}</h3>
-      </Link>
-      <p>{amount} - {createdAt}</p>
-    </div>
-  );
+const ExpenseListItem = ({id, description, amount, createdAt}) => {
+    numeral.locale('eu');
+//    render(){
+      return (
+          <div>
+            <Link to={`/edit/${id}`}>
+              <h3>{description}</h3>
+            </Link>
+            <p>
+              { 
+                numeral(amount / 100).format('$ #,##0.00')} 
+              - 
+              {moment(createdAt).format('MMMM Do, YYYY')}
+            </p>
+          </div>
+        )
+//    };
+};
 
   export default ExpenseListItem;
---
+++ src/locales/eu.js
@@ -0,0 +1,26 @@
+// numeral.js locale configuration
+// locale : English numbers with EU currency
+import numeral from 'numeral';
+
+numeral.register('locale', 'eu', {
+    delimiters: {
+        thousands: ' ',
+        decimal: ','
+    },
+    abbreviations: {
+        thousand: 'k',
+        million: 'm',
+        billion: 'b',
+        trillion: 't'
+    },
+    ordinal: function (number) {
+        var b = number % 10;
+        return (~~ (number % 100 / 10) === 1) ? 'th' :
+            (b === 1) ? 'st' :
+            (b === 2) ? 'nd' :
+            (b === 3) ? 'rd' : 'th';
+    },
+    currency: {
+        symbol: '€'
+    }
+});
